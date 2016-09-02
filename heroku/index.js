a0014d1c6cc1d754afe7df57c453eeeab3f56ad3
adidibmx <adidibmx@hotmail.com>
2016-09-02 23:59:18 +0200

    Update index.js

--- heroku/index.js
+++ heroku/index.js
@@ -32,7 +32,7 @@ class BetterSmoochApiBot extends SmoochApiBot {
 }
 
 const name = 'Charlie';
-const avatarUrl = 'https://s.gravatar.com/avatar/f91b04087e0125153623a3778e819c0a?s=80';
+const avatarUrl = 'https://raw.githubusercontent.com/adidibmx/EstherBot/master/img/charlie.jpg';
 const store = new SmoochApiStore({
     jwt
 });
