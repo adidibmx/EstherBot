da0b38059d1b970dc3aea22377ac1c47fbd681ec
adidibmx <adidibmx@hotmail.com>
2016-09-07 15:42:38 +0200

    Update script.js

--- script.js
+++ script.js
@@ -23,8 +23,8 @@ module.exports = new Script({
         receive: (bot, message) => {
             const name = message.text;
             return bot.setProp('name', name)
-                .then(() => bot.say(`Very nice to meet you ${name}
-Would you like to get to know Adrian? %[Yes](postback:yes) %[No](postback:no)`))
+                .then(() => bot.say(`Very nice to meet you ${name}.
+Would you like to get to know Adrian? %[Yes](postback:yes) %[No, thanks](postback:no)`))
                 .then(() => 'speak');
         }
     },
